ad8461990117315a400fd6d5051643ecf1531e29
M-Bahy <mohamedayman2942002@gmail.com>
2025-07-13 02:01:11 +0300

    Add __init__.py to initialize the first_scripts package

---
+++ first_scripts/__init__.py
@@ -0,0 +1,7 @@
+from .activation import *
+from .ConvLayer import *
+from .FirstModel import *
+from .FlattenLayer import *
+from .PoolingLayer import *
+from .ReLU import *
+from .KMeans import KMeans as KMeansScratch
